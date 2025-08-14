30f7377d8cd82d5333af7e6c3cd12852803bc5af
Techxie-bc <donnaogbu@gmail.com>
2025-08-14 14:00:50 +0100

    old files removed

--- src/app/old/[slug]/NewsHeroSection.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NewsHeroSection() {
-  return (
-    <section className="relative w-full h-[400px] md:h-[380px] flex items-end justify-center bg-gradient-to-b from-green-900 via-green-700 to-green-400 overflow-hidden">
-      <div className="absolute inset-0 bg-[url('/images/gradient.png')] bg-cover bg-center z-0 opacity-80" />
-    </section>
-  );
-} 
