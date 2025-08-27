c088248b5186ff282120eaa89aea058584e3b788
Joshua-Ogbonna <devjaykes@gmail.com>
2025-08-27 16:01:53 +0100

    chore: work

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-  apps : [{
+  apps: [{
     script: 'npm start',
   }],
-
-  deploy : {
-    production : {
+  deploy: {
+    production: {
       key: 'key.pem',
-      user : 'ubuntu',
-      host : '3.134.82.51',
-      ref  : 'origin/main',
-      repo : 'git@github.com:IDCLofficial/women-affairs.git',
-      path : '/home/ubuntu',
+      user: 'ubuntu',
+      host: '3.134.82.51',
+      ref: 'origin/main',
+      repo: 'git@github.com:IDCLofficial/women-affairs.git',
+      path: '/home/ubuntu',
       'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
