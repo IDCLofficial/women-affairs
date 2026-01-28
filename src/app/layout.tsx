fa0376a495ecc31b6787633f9ea6e9385982ca49
Cody998 <markhenryo368@gmail.com>
2026-01-28 02:57:16 +0100

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,46 @@ import GlobalLoader from '@/components/GlobalLoader'
 
 
 
+// optimized metadata for seo
 export const metadata: Metadata = {
-  title: "Ministry of Women Affairs and Social Welfare",
-  description: "The Ministry of Women Affairs and Social Welfare in Imo State is responsible for overseeing and developing the state's social welfare sector, as well as managing women empowerment programs.",
-};
+  title: {
+    default: 'Imo State Ministry of WomenAffairs',
+    template: '%s | Imo State Ministry of WomenAffairs'
+  },
+  description: 'Official website of the Imo State Ministry of WomenAffairs - providing services, information, and support for women across Imo State, Nigeria.',
+  keywords: ['Imo State WomenAffairs', 'Ministry of WomenAffairs', 'WomenAffairs Nigeria', 'WomenAffairs Services Imo'],
+  openGraph: {
+    type: 'website',
+    locale: 'en_NG',
+    url: 'https://womenaffairs.im.gov.ng/',
+    siteName: 'Imo State Ministry of WomenAffairs',
+    title: 'Imo State Ministry of WomenAffairs',
+    description: 'Official website of the Imo State Ministry of WomenAffairs',
+    images: [
+      {
+        url: 'https://womenaffairs.im.gov.ng/images/IMSG-Logo.svg',
+        width: 1200,
+        height: 630,
+        alt: 'Imo State Ministry of WomenAffairs',
+      }
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  verification: {
+    google:'U5rZOFb23XWMBmSdhhZaxAxPxfMqdQPXo-LyXsNVOKk',
+  },
+}
+
 
 export default function RootLayout({
   children,
